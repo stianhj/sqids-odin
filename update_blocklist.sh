a4ee0290b4eba7c599b3bdabb60705e5d26ed300
Stian H. Johannesen <stianhj@gmail.com>
2025-07-25 16:56:44 +0200

    add update blocklist helper

---
+++ update_blocklist.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+tmpdir=$(mktemp -d)
+curl -fL https://github.com/sqids/sqids-blocklist/raw/refs/heads/main/output/blocklist.json -o "$tmpdir"/blocklist.json
+
+blocklist=$(cat "$tmpdir/blocklist.json")
+blocklist=${blocklist:1:-1}
+
+blfile=$(
+cat <<HEREDOC
+package sqids
+
+@(rodata)
+default_blocklist := [?]string{ $blocklist }
+HEREDOC
+)
+
+echo "$blfile" > sqids/blocklist.odin
